8600f425b34ce8b98006a6d056a3236bbbb867f1
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-30 21:17:13 +0500

    child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,10 @@ const routes: Routes = [
   { path: '', redirectTo: 'documents', pathMatch: 'full' },
   { path: 'documents', component: DocumentListComponent, canActivate: [AuthGuard] },
   { path: 'upload', component: DocumentUploadComponent, canActivate: [AuthGuard] },
-  
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
   { path: 'favorites', component: FavoriteDocumentsComponent, canActivate: [AuthGuard] },
   { path: 'documents/:id', component: DocumentDetailComponent, canActivate: [AuthGuard] },
   { path: 'manage', component: ManageProfileComponent, canActivate: [AuthGuard] },
