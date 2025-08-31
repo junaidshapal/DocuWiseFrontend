eeec55713b280358e88e85c47e2efeedfaeffade
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-31 23:15:22 +0500

    routes removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,19 +7,7 @@ import { FavoriteDocumentsComponent } from './components/favorite-documents/favo
 import { DocumentDetailComponent } from './components/document-detail/document-detail.component';
 import { ManageProfileComponent } from './components/manage-profile/manage-profile.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'documents', pathMatch: 'full' },
-  { path: 'documents', component: DocumentListComponent, canActivate: [AuthGuard] },
-  { path: 'upload', component: DocumentUploadComponent, canActivate: [AuthGuard] },
-  {
-    path: '',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  },
-  { path: 'favorites', component: FavoriteDocumentsComponent, canActivate: [AuthGuard] },
-  { path: 'documents/:id', component: DocumentDetailComponent, canActivate: [AuthGuard] },
-  { path: 'manage', component: ManageProfileComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: 'documents' }
-];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
