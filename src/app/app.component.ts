01bb3f8661cf1a7eda30430f414f8f90c4d6237a
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-09-09 01:52:10 +0500

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrl: './app.component.css'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'docuwisefrontend';
+  isSidebarCollapsed = false;
+  isAuthRoute = false;
+  private authRoutes = ['/login', '/register', '/forgot-password', '/reset-password'];
+
+  constructor(private router: Router) {
+    this.router.events.pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => {
+        const url = this.router.url;
+        this.isAuthRoute = this.authRoutes.some(p => url.startsWith(p));
+      });
+  }
+
+  onToggleSidebar() {
+    this.isSidebarCollapsed = !this.isSidebarCollapsed;
+  }
 }
