280a9372f72f323e5da5baa3fcc30c3dbf7a2fa7
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-09-11 22:48:40 +0500

    favorite service to add and remove form favorites

--- src/app/Services/favorite.service.ts
+++ src/app/Services/favorite.service.ts
@@ -11,18 +11,22 @@ private baseUrl = 'https://localhost:7187/api/Favorites';
   constructor(private http: HttpClient) {}
 
   addFavorite(docId: number) {
+    console.log('[FavoriteService] addFavorite ->', docId);
     return this.http.post(`${this.baseUrl}/${docId}`, {});
   }
 
   removeFavorite(docId: number) {
+    console.log('[FavoriteService] removeFavorite ->', docId);
     return this.http.delete(`${this.baseUrl}/${docId}`);
   }
 
   getFavorites() {
+    console.log('[FavoriteService] getFavorites ->');
     return this.http.get<any[]>(this.baseUrl);
   }
 
   isFavorite(docId: number) {
+    console.log('[FavoriteService] isFavorite ->', docId);
     return this.http.get<boolean>(`${this.baseUrl}/check/${docId}`);
   }
 }
