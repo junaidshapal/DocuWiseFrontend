4ee29fba64596af697cc4eb36ce80a68b3836d05
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-10-18 22:20:48 +0500

    imports

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppModule } from './app/app.module';
+import { provideToastr } from 'ngx-toastr';
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
