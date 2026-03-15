8ee3eb1e742f47babdbf17df75fb94189c736405
junaidshapal <mughaljunaidmughal136@gmail.com>
2026-03-15 22:08:08 +0500

    hello

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -14,6 +14,16 @@ getProfile() {
   return this.http.get<UserProfile>(`${this.baseUrl}`);
 }
 
+updateProfile(data: any) {
+  return this.http.put(`${this.baseUrl}/update`, data);
+}
 
+changePassword(data: any) {
+  return this.http.put(`${this.baseUrl}/change-password`, data, { responseType: 'text' });
+}
+
+uploadProfilePicture(formData: FormData) {
+  return this.http.post(`${this.baseUrl}/upload-picture`, formData);
+}
 
 }
